80860ad1dca056c03ad9836efac2454fd43c8a3d
ItzSaqr <yrik435@mail.ru>
2024-09-13 16:16:33 +0700

    Added vial_v1

---
+++ vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC5, 0x3E, 0xBB, 0x2C, 0x81, 0x25, 0x73, 0x8B}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
